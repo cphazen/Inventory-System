674dfda95f8ecc0a66a739a92a2d7adc8e2938e2
unknown <cphazen@ufl.edu>
2015-04-19 20:07:16 -0400

    Adds/removes serial numbers based on modal list

--- public/modules/inventory/controllers/parts.client.controller.js
+++ public/modules/inventory/controllers/parts.client.controller.js
@@ -3,19 +3,95 @@
 angular.module('parts').controller('PartsController', ['$scope', '$modalInstance', 'Parts', 'partType',
     function($scope, $modalInstance, Parts, partType) {
 
-        $scope.serialNmbrs = [];
+        $scope.sn = {};
+        $scope.sn.serialNmbrs = [];
 
+        
+        var oldParts = [];
         var p = Parts.query(
             function (data) {
                 var theseParts = [];
                 var l = data.length;
                 for (var i = 0; i < l; i++) {
-                    if (data[i].Type == partType._id) { theseParts.push(data[i].serialNmbr); }
+                    if (data[i].Type == partType._id) {
+                        theseParts.push(data[i].serialNmbr);
+                        oldParts.push(data[i]);
+                    }
                 }
-                $scope.serialNmbrs = theseParts;
+                $scope.sn.serialNmbrs = theseParts;
             }
         );
+        $scope.oldParts = oldParts;
 
+
+        $scope.updateToMatch = function () {
+            var l_old = oldParts.length;
+            var l_new = $scope.sn.serialNmbrs.length;
+            var found;
+            //console.log('i run');
+            //removals
+            // for all in old array
+            for (var i = 0; i < l_old; i++) {
+                //console.log('i loop');
+                found = false;
+                //check if exists in new array
+                for (var j = 0; j < l_new; j++) {
+                    // break if found
+                    if (oldParts[i].serialNmbr === $scope.sn.serialNmbrs[j]) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                console.log(found);
+                // remove from db if not found
+                if (!found) {
+                    //console.log('i remove');
+                    oldParts[i].$remove(function () {
+                        //subtract from quantity
+                        partType.quantity -= 1;
+                        partType.$update(function (errorResponse) {
+                            $scope.error = errorResponse.data.message;
+                        });
+                    });
+                }
+            }
+
+            // add all
+            for (var i = 0; i < l_new; i++) {
+                found = false;
+                //check if exists in new array
+                for (var j = 0; j < l_old; j++) {
+                    //break if found
+                    if (oldParts[j].serialNmbr === $scope.sn.serialNmbrs[i]) {
+                        found = true;
+                        break;
+                    }
+                }
+                // add to db if not found
+                if (!found) {
+                    //console.log('i add');
+                    var part = new Parts({
+                        serialNmbr: $scope.sn.serialNmbrs[i],
+                        Type: partType._id
+                    });
+
+                    part.$save(function () {
+                        //add to quantity
+                        partType.quantity += 1;
+                        partType.$update(function (errorResponse) {
+                            $scope.error = errorResponse.data.message;
+                        });
+                    }, function (errorResponse) {
+                        $scope.error = errorResponse.data.message;
+                    });
+                }
+            }
+
+            $scope.sn.serialNmbrs = [];
+            $modalInstance.close();
+        }
+        
         //console.log(partType._id);
 
         $scope.create = function(){
@@ -38,6 +114,9 @@ angular.module('parts').controller('PartsController', ['$scope', '$modalInstance
 
             $modalInstance.close();
         };
+
+
+
 
      $scope.cancel = function() {
         $modalInstance.dismiss('cancel');
